b9372b4dc75ffc2aa7737798bf923ce1af30d3ac
serefarikan <serefarikan@gmail.com>
2018-01-18 17:58:49 +0000

    update

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 echo 'installing third party jars to maven repository'
 
-./install_3rd_party_jars.sh
+./install_3rd_party_jars.sh | tee install_jars.output
+grep -Fq "FAIL" install_jars.output
+if [$?]
+then
+exit 1
+fi
 echo 'finished installing third party jars'
